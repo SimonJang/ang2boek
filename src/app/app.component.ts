47b748b2850f24ddd704bb23f8b14c0459be52b5
Simon <simon.jang.contact@gmail.com>
2017-02-22 19:50:11 +0100

    eerste upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+// Declares the class representing the module/component
+export class AppComponent {
+  name = 'Simon';
+}
